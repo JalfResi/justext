5ebeb19dca531980371e56d1a963a45b2f0d35ab
Ben Davies <omarshariffdontlikeit@gmail.com>
2012-03-06 22:16:46 +0000

    Added additional flags to the command line example

--- src/example/main.go
+++ src/example/main.go
@@ -8,16 +8,32 @@ import (
 )
 
 var(
-	noHeadings *bool = flag.Bool("no-headings", false, "Ignores headings in output")
-	outputMode *bool = flag.Bool("detailed", false, "Generates HTML with detailed results")
-	debugMode  *bool = flag.Bool("debug", false, "Log debug messages")
+	lengthLow  *int          = flag.Int("length-low", 70, "Sets the length low")
+	lengthHigh *int          = flag.Int("length-high", 200, "Sets the length high")
+	stoplistLanguage *string = flag.String("language", "English", "Sets the stoplist language")
+	stopwordsLow  *float64   = flag.Float64("stopwords-low", 0.30, "Sets the stopwords low")
+	stopwordsHigh *float64   = flag.Float64("stopwords-high", 0.32, "Sets the stopwords high")
+	maxLinkDensity *float64  = flag.Float64("max-link-density", 0.2, "Sets the max link density")
+	maxHeadingDistance *int  = flag.Int("max-heading-distance", 200, "Sets the max heading distance")
+	noHeadings *bool         = flag.Bool("no-headings", false, "Ignores headings in output")
+	outputMode *bool         = flag.Bool("detailed", false, "Generates HTML with detailed results")
+	debugMode  *bool         = flag.Bool("debug", false, "Log debug messages")
 )
 
 func main() {
 	flag.Parse()
 
 	jusText := gojustext.NewReader(os.Stdin)
-	jusText.NoHeadings = *noHeadings
+
+	jusText.LengthLow          = *lengthLow
+	jusText.LengthHigh         = *lengthHigh
+	jusText.StoplistLanguage   = *stoplistLanguage		
+	jusText.StopwordsLow       = *stopwordsLow
+	jusText.StopwordsHigh      = *stopwordsHigh
+	jusText.MaxLinkDensity     = *maxLinkDensity	
+	jusText.MaxHeadingDistance = *maxHeadingDistance
+	jusText.NoHeadings         = *noHeadings	
+
 	p, err := jusText.ReadAll()
 	if err != nil {
 		log.Fatal(err)
